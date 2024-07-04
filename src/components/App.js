7f986d58283f97f8a701eccd8b95f3b820c1b890
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-07-04 08:41:55 +0000

    updating file

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ const App = () => {
   function renderContent() {
     switch(isActive) {
       case 1:
-        return <div>This is the content for tab 1.</div>
+        return <div>This is the content for Tab 1.</div>
       case 2:
-        return <div>This is the content for tab 2.</div>
+        return <div>This is the content for Tab 2.</div>
       case 3:
-        return <div>This is the content for tab 3.</div>
+        return <div>This is the content for Tab 3.</div>
       default:
         return null;
     }
